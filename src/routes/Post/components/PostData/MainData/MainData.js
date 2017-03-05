3b06332d3812df2d1efb2bb038fc30565c81e650
Matthieu <matthieubessol@gmail.com>
2017-03-06 00:23:33 +0100

    working fusion

--- src/routes/Post/components/PostData/MainData/MainData.js
+++ src/routes/Post/components/PostData/MainData/MainData.js
@@ -4,6 +4,7 @@ import "./MainData.scss";
 import PreviewActions from '../../../../../components/ListPost/PostPreview/PreviewActions/PreviewActions.js'
 
 export const MainData = (props) => {
+    console.log(props.data.title)
     let dataLink = (<div/>);
     if(props.data.type == "link")
         dataLink = (<a target="_blank" href={props.data.url} className="data__link">Voir le lien</a>)
@@ -12,9 +13,8 @@ export const MainData = (props) => {
         <div className="data">
             <div className="data__author">{props.data.date} par {props.data.author}</div>
             <div className="data__title">{props.data.title}</div>
-            <div className="data__description">{props.data.description}</div>
+            <div className="data__description">{props.data.text}</div>
             {dataLink}
-            <Tags data={props.data.tags}/>
             <PreviewActions data={props.data}/>
         </div>
     )
--- src/routes/Post/components/PostView.js
+++ src/routes/Post/components/PostView.js
@@ -1,22 +1,23 @@
-import React from 'react'
+/*import React from 'react'
+import ReactDOM from "react-dom";
 import './PostView.scss'
 import '../../Tags/components/TagView.scss'
 import PostData from './PostData/PostData.js'
 import ListComment from '../../../components/ListComment/ListComment.js'
 import Filter from '../../../components/Filter/Filter.js'
 
-const dataPost = {
-    "id":1,
-    "type":"link",
-    "url":"http://vignette3.wikia.nocookie.net/logopedia/images/f/f5/Sprite_logo2.jpg/revision/latest?cb=20140618132523",
-    "title":"Super title putaclic sa mère",
-    "description":"Sa darrone ils boivent du sprite sa mère",
-    "salt":18,
-    "pepper":20,
-    "date":"12 jan. 2017",
-    "author":"Thomas Lerouô",
-    "tags":["boisson","mere"]
-}
+// const dataPost = {
+//     "id":1,
+//     "type":"link",
+//     "url":"http://vignette3.wikia.nocookie.net/logopedia/images/f/f5/Sprite_logo2.jpg/revision/latest?cb=20140618132523",
+//     "title":"Super title putaclic sa mère",
+//     "description":"Sa darrone ils boivent du sprite sa mère",
+//     "salt":18,
+//     "pepper":20,
+//     "date":"12 jan. 2017",
+//     "author":"Thomas Lerouô",
+//     "tags":["boisson","mere"]
+// }
 
 const dataComments = [
   {
@@ -62,18 +63,83 @@ const dataComments = [
 //     "tags":["boisson","mere"]
 // }
 
-export const PostView = () => (
+let dataPost = {}
+const loadData = () => {
+    fetch('http://localhost:8888/salt-im/api/p/1')
+    .then(function(response) {
+      dataPost = response.text();
+      return dataPost;
+    })
+    .then(function(data) {
+        dataPost = data;
+    })
+}
 
-  <div className="post">
-    <PostData data={dataPost}/>
+loadData();
 
-    <div className="post__commentBlock center">
-        <p className="tagview__titleAll">Commentaires</p>
-        <Filter/>
-        <ListComment data={dataComments}/>
-    </div>
 
-  </div>
-)
+export const PostView = (props:dataPost) => {
+    console.log(props)
+    return(
+          <div className="post">
+            <PostData data={dataPost}/>
 
-export default PostView
+            <div className="post__commentBlock center">
+                <p className="tagview__titleAll">Commentaires</p>
+                <Filter/>
+                <ListComment data={dataComments}/>
+            </div>
+
+          </div>
+    )
+}
+
+export default PostView*/
+
+
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import './PostView.scss'
+import '../../Tags/components/TagView.scss'
+import PostData from './PostData/PostData.js'
+import ListComment from '../../../components/ListComment/ListComment.js'
+import Filter from '../../../components/Filter/Filter.js'
+
+class PostView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        postdata:{},
+    };
+  }
+
+  componentDidMount() {
+    let self=this;
+      fetch('http://localhost:8888/salt-im/api/p/'+self.props.params.postId)
+      .then(function(response) {
+        let dataPost = response.json();
+        return dataPost;
+      })
+      .then(function(data) {
+        self.setState({postdata:data});
+      })
+  }
+
+  render() {
+    console.log(this.state.postdata)
+    return (
+      <div className="post">
+        <PostData data={this.state.postdata}/>
+
+        <div className="post__commentBlock center">
+            <p className="tagview__titleAll">Commentaires</p>
+            <Filter/>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default PostView;
