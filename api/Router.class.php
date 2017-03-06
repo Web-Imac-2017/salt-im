20b22dd3c41918d88a7170cbfa4de99c51e81fa5
AntoineMi <antoinemiaud@gmail.com>
2017-03-06 17:05:43 +0100

    include commentController and create route

--- api/Router.class.php
+++ api/Router.class.php
@@ -23,6 +23,7 @@
 
 require "controller/indexController.php";
 require "controller/postController.php";
+require "controller/commentController.php";
 
 class Router
 {
--- api/index.php
+++ api/index.php
@@ -12,6 +12,7 @@ $router->setDefaultControllerAction('accueil','index');
 // En cas d'url invalid on appèlera le controller errorController et sa méthode alert()
 $router->setErrorControllerAction('error', 'alert'); 
 $router->addRule('p/:id', array('controller' => 'post', 'action' => 'index'));
+$router->addRule('p/comment/:id', array('controller' => 'comment', 'action' => 'index'));
 $router->load();
 
 ?>
