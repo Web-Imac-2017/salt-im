377db233548642200261444bdda091682eb5a5a3
Tencap <noisiv@outlook.fr>
2017-03-09 18:19:27 +0100

    fetch is now using method POST + refactor function for ES6 support

--- src/routes/Post/components/PostView.js
+++ src/routes/Post/components/PostView.js
@@ -1,102 +1,3 @@
-/*import React from 'react'
-import ReactDOM from "react-dom";
-import './PostView.scss'
-import '../../Tags/components/TagView.scss'
-import PostData from './PostData/PostData.js'
-import ListComment from '../../../components/ListComment/ListComment.js'
-import Filter from '../../../components/Filter/Filter.js'
-
-// const dataPost = {
-//     "id":1,
-//     "type":"link",
-//     "url":"http://vignette3.wikia.nocookie.net/logopedia/images/f/f5/Sprite_logo2.jpg/revision/latest?cb=20140618132523",
-//     "title":"Super title putaclic sa mère",
-//     "description":"Sa darrone ils boivent du sprite sa mère",
-//     "salt":18,
-//     "pepper":20,
-//     "date":"12 jan. 2017",
-//     "author":"Thomas Lerouô",
-//     "tags":["boisson","mere"]
-// }
-*/
-const dataComments = [
-  {
-    "user":"Jean Yves",
-    "userPic":"http://www.geekqc.ca/wp-content/uploads/2016/11/maxresdefault-8.jpg",
-    "date":"03 fév. 2017, 9h54",
-    "message":"Ceci est une commentaire salé et non constructif",
-    "salt":"27"
-  },
-  {
-    "user":"Christopher Lassus",
-    "userPic":"http://www.geekqc.ca/wp-content/uploads/2016/11/maxresdefault-8.jpg",
-    "date":"06 fév. 2017, 12h33",
-    "message":" Being angry, agitated, upset. William Beckett(After his Sweater Shrinks): After all of that stressing about what we were going to wear and going shopping and scrambling. I'm a little salty.",
-    "salt":"27",
-    "answers": [
-        {
-            "user":"Jean Yves",
-            "userPic":"http://www.geekqc.ca/wp-content/uploads/2016/11/maxresdefault-8.jpg",
-            "date":"03 fév. 2017, 9h54",
-            "message":"Ceci est une commentaire salé et non constructif",
-            "salt":"27"
-        },
-        {
-            "user":"Jean Yves",
-            "userPic":"http://www.geekqc.ca/wp-content/uploads/2016/11/maxresdefault-8.jpg",
-            "date":"03 fév. 2017, 9h54",
-            "message":"Ceci est une commentaire salé et non constructif",
-            "salt":"27"
-        }
-    ]
-  }
-]
-
-// dataPost = {
-//     "id":1,
-//     "type":"video",
-//     "url":"https://www.youtube.com/watch?v=_dux_Ugs2lk",
-//     "title":"Titre putaclic de sa mère qui boit du sprite",
-//     "description":"Sa darrone ils boivent du sprite sa mère",
-//     "salt":18,
-//     "pepper":20,
-//     "tags":["boisson","mere"]
-// }
-
-// let dataPost = {}
-// const loadData = () => {
-//     fetch('http://localhost:8888/salt-im/api/p/1')
-//     .then(function(response) {
-//       dataPost = response.text();
-//       return dataPost;
-//     })
-//     .then(function(data) {
-//         dataPost = data;
-//     })
-// }
-
-// loadData();
-
-
-// export const PostView = (props:dataPost) => {
-//     console.log(props)
-//     return(
-//           <div className="post">
-//             <PostData data={dataPost}/>
-
-//             <div className="post__commentBlock center">
-//                 <p className="tagview__titleAll">Commentaires</p>
-//                 <Filter/>
-//                 <ListComment data={dataComments}/>
-//             </div>
-
-//           </div>
-//     )
-// }
-
-// export default PostView
-
-
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import './PostView.scss'
@@ -105,7 +6,7 @@ import PostData from './PostData/PostData.js'
 import ListComment from '../../../components/ListComment/ListComment.js'
 import Filter from '../../../components/Filter/Filter.js'
 
-class PostView extends Component {
+export default class PostView extends Component {
   constructor(props) {
     super(props);
 
@@ -115,17 +16,12 @@ class PostView extends Component {
   }
 
   componentDidMount() {
-    
-    const myInit = {method: 'GET', mode: 'no-cors', guard: 'request-no-cors'};
 
-    fetch('http://localhost:8888/salt-im/api/p/'+this.props.params.postId, myInit)
-    .then(function(response) {
-      let dataPost = response.json();
-      return dataPost;
-    })
-    .then(function(data) {
-      this.setState({postdata:data});
-    })
+    const myInit = {method: 'POST'};
+
+    fetch('http://localhost/salt-im/api/p/'+this.props.params.postId, myInit)
+      .then((response) => response.json())
+      .then((object) => {this.setState(postdata: object)})
   }
 
   render() {
@@ -143,5 +39,3 @@ class PostView extends Component {
     );
   }
 }
-
-export default PostView;
