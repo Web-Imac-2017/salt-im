8fea87cdaee4ccb37f199337524284ee3a3ad1d7
mfruz <barbier.marie.lou@gmail.com>
2017-03-11 23:50:38 +0100

    fixed sync issues

--- api/controller/UsersManager.php
+++ api/controller/UsersManager.php
@@ -35,64 +35,66 @@
  
    public function get($id)
    {
--    // Exécute une requête de type SELECT avec une clause WHERE, et retourne un objet User.
--    $id = (int) $id;
--
--    $q = $this->_db->query('SELECT id, mail, username, password, avatar, birthDate, rank, signupDate FROM user WHERE id = '.$id);
--    $donnees = $q->fetch(PDO::FETCH_ASSOC);
--
--    return new User($donnees);
--  }
--
--
--  public function getStat(User $user) {
--    $stats = [];
+    // Exécute une requête de type SELECT avec une clause WHERE, et retourne un objet User.
+    $id = (int) $id;
+
+    $q = $this->_db->query('SELECT id, mail, username, password, avatar, birthDate, rank, signupDate FROM user WHERE id = '.$id);
+    $donnees = $q->fetch(PDO::FETCH_ASSOC);
+
+    return new User($donnees);
+  }
+
+
+  public function getStat(User $user) {
+    $stats = [];
      
--    $q = $this->_db->query('SELECT * FROM stat WHERE related_element_id = "'.$user->get_id().'"');
--      
--    while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
--    {
--      $stats[] = new Stat($donnees);
--    }
--
--    return $stats;
--}
--
--public function getSubjects(User $user) {
--    $subject_id_array = [];
--    $subjects = [];
--      
--    $q = $this->_db->query('SELECT id FROM publication JOIN rel_tag_publication ON publication.id = rel_tag_publication.publication_id WHERE rel_tag_publication.tag_id = "'.$tag_id.'"');
--      
--    // On a récupéré les ids des publications ayant le tag précisé
--      for($i=0; $row = $q->fetch(); $i++){
--        $subject_id_array[] = $row['id'];
--      }
--    // Il faut récupérer les subjects correspondant aux ids
--      
--    for($i=0; count($subject_id_array); i++) {
--        $q = $this->_db->query('SELECT * FROM subject WHERE id = "'.$subject_id_array[$i].'"');
--        while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
--        {
--            $subjects[] = new Subject($donnees);
--        }
--    }    
--
--    return $subjects;
--  }
--  public function getList()
--  {
--    // Retourne la liste de tous les users.
--    $users = [];
--
--    $q = $this->_db->query('SELECT id, mail, username, password, avatar, birthDate, rank, signupDate FROM user ORDER BY id');
--
--    while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
--    {
--      $users[] = new User($donnees);
--    }
--
--    return $users;
+    $q = $this->_db->query('SELECT * FROM stat WHERE related_element_id = "'.$user->get_id().'"');
+      
+    while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+   {
+      $stats[] = new Stat($donnees);
+    }
+
+    return $stats;
+}
+
+public function getSubjects(User $user) {
+    $subject_id_array = [];
+    $subjects = [];
+      
+    $q = $this->_db->query('SELECT id FROM publication JOIN rel_tag_publication ON publication.id = rel_tag_publication.publication_id WHERE rel_tag_publication.tag_id = "'.$tag_id.'"');
+    $q = $this->_db->query('SELECT id FROM publication WHERE user_id = AND ')
+    // On a récupéré les ids des publications ayant le tag précisé
+      for($i=0; $row = $q->fetch(); $i++){
+        $subject_id_array[] = $row['id'];
+      }
+   // Il faut récupérer les subjects correspondant aux ids
+      
+    for($i=0; count($subject_id_array); i++) {
+        $q = $this->_db->query('SELECT * FROM subject WHERE id = "'.$subject_id_array[$i].'"');
+        while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+        {
+            $subjects[] = new Subject($donnees);
+        }
+    }    
+
+    return $subjects;
+  }
+
+
+  public function getList()
+  {
+    // Retourne la liste de tous les users.
+    $users = [];
+
+    $q = $this->_db->query('SELECT id, mail, username, password, avatar, birthDate, rank, signupDate FROM user ORDER BY id');
+
+    while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+    {
+      $users[] = new User($donnees);
+    }
+
+    return $users;
    }
  
    public function update(User $user)
