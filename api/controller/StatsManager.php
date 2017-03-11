4b5e289e059c41c8bf36bc85202b6086fc1865b3
AntoineMi <antoinemiaud@gmail.com>
2017-03-11 16:52:37 +0100

    added Stat functions to retreive it entirely from the DB

--- api/controller/StatsManager.php
+++ api/controller/StatsManager.php
@@ -10,24 +10,24 @@ class StatsManager {
   public function add(Stat $stat)
   {
     // Préparation de la requête d'insertion.
-    $q = $this->_db->prepare('INSERT INTO stat(name, value) VALUES("'.$stat->get_name().'", "'.$stat->get_value().'")');
+    $q = $this->_db->prepare('INSERT INTO stat(name, value, related_element_id, related_element_type) VALUES("'.$stat->get_name().'", "'.$stat->get_value().'", "'.$stat->get_related_element_id().'", "'.$stat->get_related_element_type().'")');
     
     // Exécution de la requête.
     $q->execute();
   }
 
-  public function delete(Stat $stat)
+  /*public function delete(Stat $stat)
   {
     // Exécute une requête de type DELETE.
       $this->_db->exec('DELETE FROM stat WHERE id = "'.$stat->get_id()."'");
-  }
+  }*/
 
   public function get($id)
   {
     // Exécute une requête de type SELECT avec une clause WHERE, et retourne un objet stat.
     $id = (int) $id;
 
-    $q = $this->_db->query('SELECT id, name, value FROM stat WHERE id = "'.$id.'"');
+    $q = $this->_db->query('SELECT id, name, value, related_element_id, related_element_type FROM stat WHERE id = "'.$id.'"');
     $donnees = $q->fetch(PDO::FETCH_ASSOC);
 
     return new Stat($donnees);
@@ -38,7 +38,7 @@ class StatsManager {
     // Retourne la liste de tous les stats.
     $stats = [];
 
-    $q = $this->_db->query('SELECT id, name, value FROM stat ORDER BY id');
+    $q = $this->_db->query('SELECT id, name, value, related_element_id, related_element_type FROM stat ORDER BY id');
 
     while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
     {
@@ -51,7 +51,7 @@ class StatsManager {
   public function update(Stat $stat)
   {
     // Prépare une requête de type UPDATE.
-    $q = $this->_db->prepare('UPDATE stat SET name = "'.$stat->get_name().'", value = "'.$stat->get_value().'" WHERE id = "'.$stat->get_id().'"');
+    $q = $this->_db->prepare('UPDATE stat SET name = "'.$stat->get_name().'", value = "'.$stat->get_value().'", related_element_id = "'.$stat->get_related_element_id().'", related_element_type = "'.$stat->get_related_element_type().'" WHERE id = "'.$stat->get_id().'"');
       
     // Exécution de la requête.
     $q->execute();
