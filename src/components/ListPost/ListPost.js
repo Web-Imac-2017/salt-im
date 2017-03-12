1485476fa339489b42467d355b4d35d1795ea20c
PapyKent <kentdu38@gmail.com>
2017-03-12 20:49:57 +0100

    tag solo page finished

--- src/components/ListPost/ListPost.js
+++ src/components/ListPost/ListPost.js
@@ -3,14 +3,19 @@ import './ListPost.scss'
 import PostPreview from './PostPreview/PostPreview.js'
 
 export const ListPost = (props) => {
+ if(!props.data.length)
+        return (<div className="listpost--empty">T'es comme le "ç" de sel, t'existe pas</div>)
+    let postsNode = props.data.map( (elmt,i) => (
+        <PostPreview key={i} data={elmt}/>
+    ))
 
-    
-            return(
-                <div className="listpost">
-                    Chargement des tags
-                </div>
-            )
-        
-    }
+    return (
+        <div className="listpost">
+          <div className="listpost__postwrapper">
+            {postsNode}
+          </div>
+        </div>
+    )
+}
 
-    export default ListPost
+export default ListPost
---
+++ src/routes/Tag/components/TagData/TagData.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export const TagData = (props) => {
+    let nodeData;
+    console.log(props.data);
+
+    return(
+      <div className="tagdata">       
+           {nodeData}         
+      </div>
+    )
+}
+
+export default TagData
