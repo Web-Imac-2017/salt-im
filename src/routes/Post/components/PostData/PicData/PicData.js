582440f453dc4394d929d86c25c66ad6c81f1520
unknown <baptisteolivier@hotmail.fr>
2017-03-12 23:32:06 +0100

    modal video & picture in post

--- src/routes/Post/components/PostData/PicData/PicData.js
+++ src/routes/Post/components/PostData/PicData/PicData.js
@@ -3,6 +3,18 @@ import MainData from '../MainData/MainData.js'
 import './PicData.scss'
 
 export default class PicData extends Component {
+
+    constructor(props){
+        super(props);
+            this.state = {
+                isModalActive : false
+            }   
+    }
+
+    handleClick() {
+        this.setState({isModalActive:true})
+    }
+
     handleLoad() {
         let width = this.refs.pic.offsetWidth
         this.refs.pic.style.height = width + "px";
@@ -10,11 +22,24 @@ export default class PicData extends Component {
     render() {
         if(!this.props.data)
             return (<div/>)
+        let classes = 'modal';
+
+        if (this.state.isModalActive)
+            classes += ' modal--active';
+
+
         return(
             <div className="picdata flex">
-                <div ref="pic" className="picdata__left flex-3" style={{backgroundImage: 'url(' + this.props.dataMedia.link + ')'}}>
+                <div onClick={this.handleClick.bind(this)} ref="pic" className="picdata__left flex-3" style={{backgroundImage: 'url(' + this.props.dataMedia.link + ')'}}>
                     <img style={{display:"none"}} src={this.props.dataMedia.link} onLoad={this.handleLoad.bind(this)}/>
                 </div>
+
+                <div className={classes}>
+                    <div className='modal__wrapper'>
+                        <img src={this.props.dataMedia.link}/>
+                    </div>
+                </div>    
+
                 <div className="picdata__right flex-7">
                     <MainData data={this.props.data} dataUser={this.props.dataUser} nbComment={this.props.nbComment}/>
                 </div>
--- src/routes/Post/components/PostData/VideoData/VideoData.js
+++ src/routes/Post/components/PostData/VideoData/VideoData.js
@@ -3,6 +3,17 @@ import MainData from '../MainData/MainData.js'
 
 export default class VideoData extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            isModalActive : false
+        }
+    }
+
+    handleClick() {
+        this.setState({isModalActive:true})
+    }    
+
     youtube_parser(url){
         let regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
         let match = url.match(regExp);
@@ -13,11 +24,18 @@ export default class VideoData extends Component {
         let ytUrl = this.youtube_parser(this.props.dataMedia.link);
         return(
             <div className="videodata flex">
-                <div className="videodata__left flex-3">
+                <div onClick={this.handleClick.bind(this)} className="videodata__left flex-3">
                     <div className="iframeWrapper">
                         <iframe src={"https://www.youtube.com/embed/"+ytUrl+"?ecver=2"} width="480" height="360" frameborder="0" allowfullscreen></iframe>
                     </div>
                 </div>
+
+                <div className={classes}>
+                    <div className="iframeWrapper">
+                        <iframe src={"https://www.youtube.com/embed/"+ytUrl+"?ecver=2"} width="480" height="360" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>  
+
                 <div className="videodata__right flex-7">
                     <MainData data={this.props.data} dataUser={this.props.dataUser} nbComment={this.props.nbComment}/>
                 </div>
