388b5c82c4b7405c4b30dd276277ce604740346e
Matthieu <matthieubessol@gmail.com>
2017-03-12 21:29:42 +0100

    fix items

--- src/components/ListTag/column/ListTagColumn.js
+++ src/components/ListTag/column/ListTagColumn.js
@@ -6,21 +6,21 @@ import Tag from '../Tag/Tag.js'
 
 export const ListTagColumn = (props) => {
     if(props.data.map != undefined  ){
-        let tagsNode = props.data.map( (elmt,i) => (
-            <Tag key={i} data={elmt}/>
-            ))
+        let tagsNode = props.data.map( (elmt,i) => {
+            if(i < 10)
+                return (<Tag key={i} data={elmt}/>);
+        })
 
-            return (
-                <div className="tagsColumn">        
+        return (
+            <div className="tagsColumn">
                 <div className="tagsColumn__list">
                     {tagsNode}
                 </div>
-                </div>
-            )
+            </div>
+        )
     }
 
-   else
-    {
+   else {
         return (
             <div>Chargement des tags</div>
         )
