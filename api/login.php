7585c6674b28ae5e2c98d29125632ee863fa0e49
Judgingu <flavie.llucas@gmail.com>
2017-03-13 17:24:29 +0100

    Forgot a function because I'm dumb

--- api/login.php
+++ api/login.php
@@ -1,5 +1,31 @@
 
 <?php
+public function login($uname,$umail,$upassword)
+    {
+       try
+       {
+          $stmt = $this->db->prepare("SELECT * FROM user WHERE username=:uname OR mail=:umail LIMIT 1");
+          $stmt->execute(array(':uname'=>$uname, ':umail'=>$umail));
+          $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+          if($stmt->rowCount() > 0)
+          {
+             if(password_verify($upass, $userRow['user_password']))
+             {
+                $_SESSION['user_session'] = $userRow['user_id'];
+                return true;
+             }
+             else
+             {
+                return false;
+             }
+          }
+       }
+       catch(PDOException $e)
+       {
+           echo $e->getMessage();
+       }
+   }
+
 require_once 'connect.php';
 
 if($user->is_logged_in()!="")
