4f96cec28e5b7f5abe1e15bcbcacd19043fe392a
unknown <baptisteolivier@hotmail.fr>
2017-03-13 20:21:46 +0100

    big search results in static almost complete

--- src/components/Header/BigSearch/BigSearch.js
+++ src/components/Header/BigSearch/BigSearch.js
@@ -8,7 +8,12 @@ class BigSearch extends Component {
 
     this.state = {
         isOpen:false,
+        search:"",
     };
+  }
+
+  handleChangeSearch(event){
+    this.setState({search: event.target.value});
   }
 
   componentWillReceiveProps(nextProps) {
@@ -19,14 +24,79 @@ class BigSearch extends Component {
 
   render() {
     let classes = "bigsearch";
+    let resultClass = "results";
+
     if(this.state.isOpen)
+    {
         classes += " bigsearch--open"
+    }
+
+    if(this.state.search != "")
+      resultClass +=" results--open"
+
     return (
-      <div className={classes}>
-        <form>
-            <input type="text" placeholder="Search"/>
-        </form>
+      <div>
+        <div className={classes}>
+          <form>
+              <input type="text" onChange={this.handleChangeSearch.bind(this)} placeholder="Search"/>
+          </form>
+        </div>
+
+        <div className={resultClass}>
+
+          <div className="results__fetch">
+            <div className="results__wrapper">
+              <h1> Posts </h1>
+                <ul>
+                  <li>manger des pates</li>
+                  <li>manger des anus</li>
+                  <li>manger la chatte à la voisine</li>
+                  <li>manger son sexe</li>
+                </ul>
+            </div>
+          </div>
+
+          <div className="results__fetch">
+            <div className="results__wrapper">
+              <h1> Tags </h1>
+                <ul>
+                  <li>manger des pates</li>
+                  <li>manger des anus</li>
+                  <li>manger la chatte à la voisine</li>
+                  <li>manger son sexe</li>
+                  <li>manger des pates</li>
+                  <li>manger des anus</li>
+                  <li>manger la chatte à la voisine</li>
+                  <li>manger son sexe</li>
+                  <li>manger des pates</li>
+                  <li>manger des anus</li>
+
+                </ul>
+
+                <span className="results__seeall"> Voir tout </span>
+            </div>
+          </div>
+
+          <div className="results__fetch">
+            <div className="results__wrapper">
+              <div className="results__titleline">
+                <h1 className='results__titleline__title'> Vicos </h1>
+                
+              </div>
+                <ul>
+                  <li>manger des pates</li>
+                  <li>manger des anus</li>
+                  <li>manger la chatte à la voisine</li>
+                  <li>manger son sexe</li>
+                </ul>
+
+                <span className="results__seeall"> Voir tout </span>
+            </div>
+          </div>          
+
+        </div>
       </div>
+
     );
   }
 }
