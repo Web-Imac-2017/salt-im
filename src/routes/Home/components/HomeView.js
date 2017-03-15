997de45f55d70d82b837732883cb3b900996711c
noisiv <noisiv@outlook.fr>
2017-03-15 22:53:17 +0100

    fix error fetching homeview tags

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,29 +1,6 @@
-import React, {Component} from 'react'
-import './HomeView.scss'
-import '../../Tags/components/TagView.scss'
-import ListPost from '../../../components/ListPost/ListPost.js'
-import SearchBar from '../../../components/SearchBar/SearchBar.js'
-import ListTagColumn from '../../../components/ListTag/column/ListTagColumn.js'
-import Filter from '../../../components/Filter/Filter.js'
-import utils from '../../../../public/utils.js'
+Data) => {this.setState({dataListTags : Data})})
 
-
-export default class HomeView extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            dataListPost:{},
-            dataListTags:{}
-        };
-    }
-
-    componentDidMount() {
-        fetch(utils.getFetchUrl()+"/tag/all")
-            .then((tagResponse) => tagResponse.json())
-            .then((tagData) => {this.setState({dataListTags : tagData})})
-
-        fetch("http://www.json-generator.com/api/json/get/bSmxnPKrma?indent=2")
+        fetch("http://www.json-generator.com/api/jsoccn/get/bSmxnPKrma?indent=2")
             .then((response) => response.json())
             .then((data) =>{this.setState({dataListPost : data})})
     }
