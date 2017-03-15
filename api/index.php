f9d1bd1baedee174f746afbb7c3cfebb56f29be6
noisiv <noisiv@outlook.fr>
2017-03-15 16:22:13 +0100

    php routes filtered properly

--- api/index.php
+++ api/index.php
@@ -13,22 +13,32 @@ $router->setDefaultControllerAction('accueil','index');
 $router->setErrorControllerAction('error', 'alert');
 
 // SUR MA ROUTE OUIHIHI Y A EU DU MOUV OUIHIHI
+
+// media routes
 $router->addRule('media/:id', array('controller' => 'media', 'action' => 'index'));
 $router->addRule('media/:id/img', array('controller' => 'media', 'action' => 'img'));
+
+// post routes
 $router->addRule('p/post/add/:id', array('controller' => 'post', 'action' => 'add'));
 $router->addRule('p/:id', array('controller' => 'post', 'action' => 'index'));
 $router->addRule('p/post/remove/:id', array('controller' => 'post', 'action' => 'remove'));
-$router->addRule('comment/get/:id', array('controller' => 'comment', 'action' => 'index'));
 $router->addRule('p/comment/:id/:order', array('controller' => 'comment', 'action' => 'commentsFromPost'));
 $router->addRule('p/comment/:id', array('controller' => 'comment', 'action' => 'commentsFromPostDefault'));
 $router->addRule('p/u/:id', array('controller' => 'post', 'action' => 'postFromUser'));
 $router->addRule('p/:id/stat/up', array('controller' => 'stat', 'action' => 'upVote'));
 $router->addRule('p/:id/stat/cancel', array('controller' => 'stat', 'action' => 'cancelVote'));
 $router->addRule('p/:id/stat/vote', array('controller' => 'stat', 'action' => 'voteStatus'));
+$router->addRule('p/help/:id', array('controller' => 'post', 'action' => 'help'));
+$router->addRule('p/tag', array('controller' => 'post', 'action' => 'getFromTags'));
+$router->addRule('p/:id/stat', array('controller' => 'stat', 'action' => 'getStatPost'));
+
+// comment routes
+$router->addRule('comment/get/:id', array('controller' => 'comment', 'action' => 'index'));
 $router->addRule('comment/u/:id', array('controller' => 'comment', 'action' => 'commentFromUser'));
 $router->addRule('comment/add/:id', array('controller' => 'comment', 'action' => 'add'));
 $router->addRule('comment/add/comment/:id', array('controller' => 'comment', 'action' => 'addToComment'));
-$router->addRule('p/help/:id', array('controller' => 'post', 'action' => 'help'));
+
+// user routes
 $router->addRule('u/signup/:id', array('controller' => 'user', 'action' => 'signup'));
 $router->addRule('u/signout', array('controller' => 'user', 'action' => 'signout'));
 $router->addRule('u/login/:id', array('controller' => 'user', 'action' => 'login'));
@@ -40,13 +50,13 @@ $router->addRule('u/:id', array('controller' => 'user', 'action' => 'index'));
 $router->addRule('u/get/:id', array('controller' => 'user', 'action' => 'index'));
 $router->addRule('u/:id/avatar', array('controller' => 'user', 'action' => 'avatar'));
 $router->addRule('u/islogged/:id', array('controller' => 'user', 'action' => 'islogged'));
-$router->addRule('p/tag', array('controller' => 'post', 'action' => 'getFromTags'));
+$router->addRule('u/:id/stat', array('controller' => 'stat', 'action' => 'getStatUser'));
+$router->addRule('u/session', array('controller' => 'user', 'action' => 'who_is_logged_in'));
+
+//tag routes
 $router->addRule('tag/all', array('controller' => 'tag', 'action' => 'getList'));
 $router->addRule('tag/add', array('controller' => 'tag', 'action' => 'add'));
 $router->addRule('tag/img', array('controller' => 'tag', 'action' => 'img'));
-$router->addRule('p/:id/stat', array('controller' => 'stat', 'action' => 'getStatPost'));
-$router->addRule('u/:id/stat', array('controller' => 'stat', 'action' => 'getStatUser'));
-$router->addRule('u/session', array('controller' => 'user', 'action' => 'who_is_logged_in'));
 $router->addRule('tag/:id/img', array('controller' => 'tag', 'action' => 'img'));
 
 // route de recherche qui marche pas
