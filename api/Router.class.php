5989fed7710a42b5c0fb79eb9b7ca650492637a8
Damagae <daphnegm.rose@gmail.com>
2017-03-16 23:54:15 +0100

    d

--- api/Router.class.php
+++ api/Router.class.php
@@ -177,7 +177,7 @@ class Router
         }
 
         //Inclusion du controller
-        require_once $this->file;
+        //require_once $this->file;
 
         $class      = $this->controller . 'Controller';
 
