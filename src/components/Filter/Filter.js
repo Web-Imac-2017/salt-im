927b2c15552a7b2ec27e45254a00239c918c1e09
noisiv <noisiv@outlook.fr>
2017-03-16 22:46:26 +0100

    filter post fetch added

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { IndexLink, Link } from 'react-router'
 import './Filter.scss'
 
-export const Filter = () => (
-    <div className="select">
-        <span className="select__text">Filtrer par</span>
-        <div className="select__wrap">
-	        <select className="select__input">
-	          <option>taux de sel</option>
-	          <option>taux de poivre</option>
-	          <option>taux d'humour</option>
-	          <option>date</option>
-	        </select>
-        </div>
-    </div>
-)
+export default class Filter extends Component{
+    constructor(props) {
+        super(props);
 
-export default Filter
+        this.state = {
+            value: 0
+        };
+    }
+
+    change = (e) => {
+        this.setState({idStat : e.target.value})
+    }
+
+    render(){
+        return(
+            <div className="select">
+                <span className="select__text">Filtrer par</span>
+                <div className="select__wrap">
+                    <select className="select__input" onChange={this.change} value={this.state.value}>
+                        <option value={0}>taux de sel</option>
+                        <option value={1}>taux de poivre</option>
+                        <option value={2}>taux d'humour</option>
+                        <option value={3}>date</option>
+                    </select>
+                </div>
+            </div>
+        )
+    }
+}
